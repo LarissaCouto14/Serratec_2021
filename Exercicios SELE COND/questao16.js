4ae1a40183fe2afcdf734dab9f68793d6fa3b76e
LarissaCouto14 <81425436+LarissaCouto14@users.noreply.github.com>
2021-04-09 20:32:15 -0300

    MINI DP

--- Exercicios SELE COND/questao16.js
+++ Exercicios SELE COND/questao16.js
@@ -7,3 +7,21 @@
 //c.Valor do desconto para o INSS
 //d.Salário líquido (Salário base + horas extras – desconto INSS)
 //e.Imprimir na tela o contracheque do funcionário.
+
+const ler = require("prompt-sync")();
+
+const horas = 160
+
+var valorhr = parseFloat(ler("Quanto recebe por hora? R$"));
+var inss = parseFloat(ler("Qual o valor do desconto para o INSS? "));
+var extras = Number(ler("Quantas horas extras foram feitas? Separar horas de minutos com um .: "));
+
+var salariobase = valorhr * horas
+extras = extras * valorhr
+var salarioliquido = salariobase + extras - inss
+
+console.log("C O N T R A - C H E Q U E");
+console.log("Seu salário base é: R$" + salariobase);
+console.log("O valor do desconto do INSS é: R$" + inss);
+console.log("E o seu salário liquido é: R$" + salarioliquido);
+© 2021 GitHub, Inc.
