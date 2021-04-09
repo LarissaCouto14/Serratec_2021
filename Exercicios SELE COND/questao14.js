d7d474091f5e640520a83f137332ddc3c1fddd54
LarissaCouto14 <81425436+LarissaCouto14@users.noreply.github.com>
2021-04-09 20:31:04 -0300

    Delete questao14.js

--- Exercicios SELE COND/questao14.js
+++
@@ -1,2 +0,0 @@
-A “SuperCalc” é a calculadora que, além dos números, lê o operador que será aplicado, e depois realiza a operação. 
-Considere as entradas na ordem: primeiro número, sinal (+, -, * ou /) e segundo número.
