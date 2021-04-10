4e00e5aa954bcb35c65c7ab68f8106712a578d32
LarissaCouto14 <81425436+LarissaCouto14@users.noreply.github.com>
2021-04-10 14:18:31 -0300

    Delete Exercicios 3 directory

--- Exercicios 3/Educado.js
+++
@@ -1,9 +0,0 @@
-const prompt = require ("prompt-sync")();
-
-const ler = require("prompt-sync")();
-var nome = ler ("Digite seu nome: ");
-var apelido = ler ("Como você gostaria de ser chamado? ");
-
-
-
-console.log (" Seja bem vinda " + apelido);
